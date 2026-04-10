11374cec51d5ec59548408d52191cca9a2c7315d
Ikenna Richard <oguejioforrichard@gmail.com>
2026-04-10 19:59:33 +0100

    refactor

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,26 +1,31 @@
-package api
+package main
 
 import (
 	"log"
 	"net/http"
+	"os"
 	"time"
 
-	"github.com/ikennarichard/genderize/internal/handler"
+	handler "github.com/ikennarichard/genderize/internal/api"
 )
-
 
 func main() {
     mux := http.NewServeMux()
     mux.HandleFunc("GET /api/classify", handler.Classify)
 
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+
     server := &http.Server{
-        Addr:         ":8080",
+        Addr:         ":" + port,
         Handler:      mux,
         ReadTimeout:  10 * time.Second,
         WriteTimeout: 15 * time.Second,
         IdleTimeout:  60 * time.Second,
     }
 
-    log.Println("Server starting on :8080")
+    log.Printf("Server starting on :%s", port)
     log.Fatal(server.ListenAndServe())
 }
